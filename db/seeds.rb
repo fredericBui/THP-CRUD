f3d38413e47f4d1a7a0b46a834826bfe70e082b1
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-11 13:38:41 +0100

    generate 3 random users and 5 gossips

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Gossip.destroy_all
+
+3.times do
+    $randomCharacter = Faker::TvShows::NewGirl.character
+    if $randomCharacter != User.find_by(name: $randomCharacter)
+        $user = User.create(name: $randomCharacter,email: "#{$randomCharacter}@gmail.com")
+    end
+end
+
+5.times do
+    Gossip.create(content: Faker::TvShows::NewGirl.quote, user: User.find( 1 + rand(2) ) )
+end
