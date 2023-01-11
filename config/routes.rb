46ab821309fbc810b95b9f41a9d05b1962baef5a
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-11 12:10:01 +0100

    route to contact, team and home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/contact" , to: "contact#index"
+  get :team, to: "team#index"
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
