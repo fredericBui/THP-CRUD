080ecd1597c88f6a0533fc5d7be421ba8134225b
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-11 16:24:59 +0100

    add title to gossip

--- db/migrate/20230111120541_create_gossips.rb
+++ db/migrate/20230111120541_create_gossips.rb
@@ -1,6 +1,7 @@
 class CreateGossips < ActiveRecord::Migration[7.0]
   def change
     create_table :gossips do |t|
+      t.string :title
       t.string :content
       t.references :user, null: false, foreign_key: true
 
