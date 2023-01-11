b4ee068f1f08ebd51d1e0f5f3154bd05fecc2bd1
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-11 12:18:28 +0100

    route welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/contact" , to: "contact#index"
+  get "/welcome" , to: "welcome#index"
   get :team, to: "team#index"
   # Defines the root path route ("/")
   root "home#index"
