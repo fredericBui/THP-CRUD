c44d9022a6c66408bdea4a54149fc0fbdcb94d8f
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-11 16:24:41 +0100

    refactor user migration

--- db/migrate/20230111145521_add_first_name_description_age_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddFirstNameDescriptionAgeToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :firstName, :string
-    add_column :users, :description, :text
-    add_column :users, :age, :integer
-  end
-end
