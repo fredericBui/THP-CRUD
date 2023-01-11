be484de73be0b3a308d39ea2a2b045b7ed530428
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-11 16:25:15 +0100

    refactor schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_145521) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_11_144013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_145521) do
   end
 
   create_table "gossips", force: :cascade do |t|
+    t.string "title"
     t.string "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -30,13 +31,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_145521) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "firstName"
     t.string "email"
+    t.text "description"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "city_id", null: false
-    t.string "firstName"
-    t.text "description"
-    t.integer "age"
     t.index ["city_id"], name: "index_users_on_city_id"
   end
 
