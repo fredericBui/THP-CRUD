777c119249b0fc270ae9eec0c711c54728cf320e
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-11 15:47:47 +0100

    user relate to city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_142549) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_11_144013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,7 +33,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_142549) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "city_id", null: false
+    t.index ["city_id"], name: "index_users_on_city_id"
   end
 
   add_foreign_key "gossips", "users"
+  add_foreign_key "users", "cities"
 end
