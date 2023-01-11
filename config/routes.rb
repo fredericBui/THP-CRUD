f69d5339092ddbaa0df8160401dec1eebbd198f4
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-11 15:30:33 +0100

    profil route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/contact" , to: "contact#index"
   get "/welcome" , to: "welcome#index"
+  get "/profil" , to: "profil#index"
   get :team, to: "team#index"
   # Defines the root path route ("/")
   root "home#index"
