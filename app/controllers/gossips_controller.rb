a6c69380a4669cc2a1737ff9cf2263da6bb77f72
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-12 14:39:36 +0100

    change gossip to gossips

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,49 @@
+class GossipsController < ApplicationController
+    def index
+        @gossips = Gossip.all
+    end
+
+    def show
+        @gossip = Gossip.find(params[:id])
+    end
+
+    # def new
+    #     @gossip = Gossip.new
+    # end
+
+    # def create
+    #     @gossip = Gossip.new(title: "test", content: "test", user: User.last())
+    
+    #     if @gossip.save
+    #       redirect_to @gossip
+    #     else
+    #       render :new, status: :unprocessable_entity
+    #     end
+    # end
+
+    def edit
+        @gossip = Gossip.find(params[:id])
+    end
+
+    def update
+        @gossip = Gossip.find(params[:id])
+
+        if @gossip.update(gossip_params)
+          redirect_to @gossip
+        else
+          render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @gossip = Gossip.find(params[:id])
+        @gossip.destroy
+
+        redirect_to root_path, status: :see_other
+    end
+    
+    private
+        def gossip_params
+            params.require(:gossip).permit(:title, :content)
+        end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/contact" , to: "contact#index"
   get "/welcome" , to: "welcome#index"
   get :team, to: "team#index"
 
   resources :profil
-  resources :gossip
+  resources :gossips
   resources :city
 
   # Defines the root path route ("/")
-  root "gossip#index"
+  root "gossips#index"
 end
